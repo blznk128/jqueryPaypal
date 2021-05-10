82ba05193e1786747eac619e44f60e08756b1fee
Benny Borunda <blznk128@yahoo.com>
2021-05-10 13:00:48 -0600

    added quantity

--- index.js
+++ index.js
@@ -5,7 +5,18 @@ $(document).ready(function() {
         
         let itemInfo = $(this.dataset)[0]
         itemInfo.qty = 1;
-        shopcart.push(itemInfo)
+        let itemInCart = false
+        $.each(shopcart, function(index, value){
+            // console.log(index + " " + value.id)
+            if(value.id == itemInfo.id){
+                value.qty = parseInt(value.qty) + parseInt(itemInfo.qty)
+                itemInCart = true
+            }
+        })
+        if(!itemInCart){
+            shopcart.push(itemInfo)
+        }
+        
         console.log(shopcart)
     })
 })

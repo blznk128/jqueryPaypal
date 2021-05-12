de09a161b7e32ee7e92a2aec359d5d0f2718dfad
Benny Borunda <blznk128@yahoo.com>
2021-05-12 17:18:35 -0600

    added modal

--- index.js
+++ index.js
@@ -28,17 +28,22 @@ function outputCart() {
     if(sessionStorage["sca"]!= null) {
         shopcart = JSON.parse(sessionStorage["sca"].toString())
         console.log(sessionStorage["sca"])
+        $("#checkoutdiv").show()
     }
     let holderHTML = ''
     let total = 0
+    let itemCnt = 0
     $.each(shopcart, function(index, value) {
-        console.log(value)
+        console.log('this is value: ',value)
         let stotal = value.qty * value.price
         total += stotal
+        itemCnt += value.qty
         holderHTML += '<div>Item '+ value.name + '('+value.s+')Qty ' + value.qty + 'Price' + formatMoney(value.price) +  'ID('+value.id+') subtotal = ' + formatMoney(stotal) + '</div>'
     })
     holderHTML += '<div>'+ formatMoney(total) + '</div>'
     $("#output").html(holderHTML)
+    $(".total").html(formatMoney(total))
+    $(".items").html(itemCnt)
 }
 function formatMoney(money) {
     return '$' + (money/100).toFixed(2)
